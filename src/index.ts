3753c82c6f42f1b82ba79d000e139849dd6555c8
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-06-24 00:43:02 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import { ABIRegistry, Blockchain } from '@btc-vision/btc-runtime/runtime';
 import { Moto } from './contracts/Moto';
 
 export function defineSelectors(): void {
+    /** OP_NET */
     ABIRegistry.defineGetterSelector('address', false);
     ABIRegistry.defineGetterSelector('owner', false);
     ABIRegistry.defineMethodSelector('isAddressOwner', false);
 
+    /** OP_20 */
     ABIRegistry.defineMethodSelector('allowance', false);
     ABIRegistry.defineMethodSelector('approve', true);
     ABIRegistry.defineMethodSelector('balanceOf', false);
@@ -19,6 +21,9 @@ export function defineSelectors(): void {
     ABIRegistry.defineGetterSelector('symbol', false);
     ABIRegistry.defineGetterSelector('totalSupply', false);
     ABIRegistry.defineGetterSelector('maxSupply', false);
+
+    /** Moto */
+    ABIRegistry.defineMethodSelector('airdrop', true);
 }
 
 Blockchain.contract = () => new Moto();
