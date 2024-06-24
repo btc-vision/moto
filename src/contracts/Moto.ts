56c9afc95e2f74eb68c64ff6cd38be594e18fba9
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-06-24 00:36:18 -0400

    Added airdrop

--- src/contracts/Moto.ts
+++ src/contracts/Moto.ts
@@ -1,5 +1,13 @@
 import { u256 } from 'as-bignum/assembly';
-import { BytesWriter, Calldata, OP_20, Selector } from '@btc-vision/btc-runtime/runtime';
+import {
+    BytesWriter,
+    Calldata,
+    encodeSelector,
+    Map,
+    OP_20,
+    Selector,
+} from '@btc-vision/btc-runtime/runtime';
+import { Address } from '@btc-vision/bsi-binary';
 
 @final
 export class Moto extends OP_20 {
@@ -16,8 +24,27 @@ export class Moto extends OP_20 {
 
     public override callMethod(method: Selector, calldata: Calldata): BytesWriter {
         switch (method) {
+            case encodeSelector('airdrop'):
+                return this.airdrop(calldata);
             default:
                 return super.callMethod(method, calldata);
         }
+    }
+
+    private airdrop(calldata: Calldata): BytesWriter {
+        const drops: Map<Address, u256> = calldata.readAddressValueTuple();
+
+        const addresses: Address[] = drops.keys();
+        for (let i: i32 = 0; i < addresses.length; i++) {
+            const address = addresses[i];
+            const amount = drops.get(address);
+
+            this._mint(address, amount);
+        }
+
+        const writer: BytesWriter = new BytesWriter();
+        writer.writeBoolean(true);
+
+        return writer;
     }
 }
