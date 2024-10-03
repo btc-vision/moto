c13c5858f5943921faef95c7edc7b246cda05288
youngslohmlife <youngslohmlife@gmail.com>
2024-10-03 23:30:55 +0000

    fix build

--- src/index.ts
+++ src/index.ts
@@ -3,28 +3,28 @@ import { Moto } from './contracts/Moto';
 
 export function defineSelectors(): void {
     /** OP_NET */
-    ABIRegistry.defineGetterSelector('address', false);
-    ABIRegistry.defineGetterSelector('owner', false);
-    ABIRegistry.defineMethodSelector('isAddressOwner', false);
+    ABIRegistry.defineMethodSelector('address');
+    ABIRegistry.defineMethodSelector('owner');
+    ABIRegistry.defineMethodSelector('isAddressOwner');
 
     /** OP_20 */
-    ABIRegistry.defineMethodSelector('allowance', false);
-    ABIRegistry.defineMethodSelector('approve', true);
-    ABIRegistry.defineMethodSelector('balanceOf', false);
-    ABIRegistry.defineMethodSelector('burn', true);
-    ABIRegistry.defineMethodSelector('mint', true);
-    ABIRegistry.defineMethodSelector('transfer', true);
-    ABIRegistry.defineMethodSelector('transferFrom', true);
+    ABIRegistry.defineMethodSelector('allowance');
+    ABIRegistry.defineMethodSelector('approve');
+    ABIRegistry.defineMethodSelector('balanceOf');
+    ABIRegistry.defineMethodSelector('burn');
+    ABIRegistry.defineMethodSelector('mint');
+    ABIRegistry.defineMethodSelector('transfer');
+    ABIRegistry.defineMethodSelector('transferFrom');
 
-    ABIRegistry.defineGetterSelector('decimals', false);
-    ABIRegistry.defineGetterSelector('name', false);
-    ABIRegistry.defineGetterSelector('symbol', false);
-    ABIRegistry.defineGetterSelector('totalSupply', false);
-    ABIRegistry.defineGetterSelector('maxSupply', false);
+    ABIRegistry.defineMethodSelector('decimals');
+    ABIRegistry.defineMethodSelector('name');
+    ABIRegistry.defineMethodSelector('symbol');
+    ABIRegistry.defineMethodSelector('totalSupply');
+    ABIRegistry.defineMethodSelector('maxSupply');
 
     /** Moto */
-    ABIRegistry.defineMethodSelector('airdrop', true);
-    ABIRegistry.defineMethodSelector('airdropDefined', true);
+    ABIRegistry.defineMethodSelector('airdrop');
+    ABIRegistry.defineMethodSelector('airdropDefined');
 }
 
 Blockchain.contract = () => new Moto();
